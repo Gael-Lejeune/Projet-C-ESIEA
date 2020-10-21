337bfe9085e2efde0db57c30e723aa24f417d3c5
angelique2000 <72751313+angelique2000@users.noreply.github.com>
2020-10-21 15:56:00 +0200

    Ajout de l'entrée du train en gare

--- mouvTrain.c
+++ mouvTrain.c
@@ -13,5 +13,24 @@ void deplacementTrain(TRAIN montrain, int col, int vitesse, int time, int posX, 
 	return;
 }
 
+void entreeTrain(TRAIN montrain, int posX, int posY, int time, int vitesse){
+	int dernierwagon=52;
+	int i=0;
+	while(dernierwagon>=0){
+		time=(time+1)%vitesse;
+		if(time==1000){
+			printf("\033[%d;%dH%c\n", posX, i, montrain.custom[1][dernierwagon]);
+			printf("\033[%d;%dH%c\n", posX+1, i, montrain.custom[1][dernierwagon]);
+			dernierwagon--;
+			i++;
+		}
+	}
+}
 
-
+/*	int time=0, col=1, posX=7, posY=0, vitesse=10000000;
+	entreeTrain(montrain, posX, posY, time, vitesse);
+	deplacementTrain(montrain, col, vitesse, time, posX, posY, 13);
+	//13 : posArret : position pour laquelle le train est correctement à quai
+	sleep(2); //temp d'arrêt du train
+	deplacementTrain(montrain, 12, vitesse, time, posX, posY, 24);
+	//12 : col : position de l'arrière du train à l'arrêt ; 24 : posArret : position pour laquelle le train est complètement dans la gare et s'apprête à en sortir */
